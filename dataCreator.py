d7079343b4c90a35d15728946c6b55d338a40e3c
Gian <giancarloprados@aluno.ufsj.edu.br>
2022-12-01 11:13:48 -0300

    sec

--- dataCreator.py
+++ dataCreator.py
@@ -1,7 +1,7 @@
 import random
 from os import system as sys
 
-sys('rm ./in/*.csv')
+sys('rm ./in/*/*.csv')
 
 possibleValues = list(range(0, 10000))
 dataSizes = [20, 50, 100, 300, 500, 5000]
@@ -10,13 +10,37 @@ sample = []
 
 for size in dataSizes:
     
-    file = open(f'./in/{size}.csv', 'x')
-    file.write(f'{size}\n')
+    randomSamples = open(f'./in/random/{size}.csv', 'x')
+    randomSamples.write(f'{size}\n')
+
+    ascendingSamples = open(f'./in/ascending/{size}.csv', 'x')
+    ascendingSamples.write(f'{size}\n')
+
+    descendingSamples = open(f'./in/descending/{size}.csv', 'x')
+    descendingSamples.write(f'{size}\n')
 
     for j in range(0, samplesNumber):
 
         sample = random.sample(possibleValues, size)
 
         for i in sample:
-            file.write(f'{i}; ')
-        file.write('\n')
+            randomSamples.write(f'{i}; ')
+        randomSamples.write('\n')
+
+    for j in range(0, samplesNumber):
+
+        sample = random.sample(possibleValues, size)
+        sample.sort()
+
+        for i in sample:
+            ascendingSamples.write(f'{i}; ')
+        ascendingSamples.write('\n')
+
+    for j in range(0, samplesNumber):
+
+        sample = random.sample(possibleValues, size)
+        sample.sort(reverse=True)
+
+        for i in sample:
+            descendingSamples.write(f'{i}; ')
+        descendingSamples.write('\n')
--- runner.py
+++ runner.py
@@ -5,55 +5,65 @@ import time
 from insertionSort import insertion_sort
 from mergeSort import merge_sort
 
+from dataCreator import samplesNumber as size
+
 # Determinação dos arquivos de teste
-sets = glob.glob('./in/*.csv')
+randomSets = glob.glob('./in/random/*.csv')
+ascendingSets = glob.glob('./in/ascending/*.csv')
+descendingSets = glob.glob('./in/descending/*.csv')
+sets = [randomSets, ascendingSets, descendingSets]
+outs = ['./out/insertion/random.csv', './out/insertion/ascending.csv', './out/insertion/descending.csv']
 
-# Criação do buffer que armazenará os dados antes da escrita.
-buffer = [[] for i in range(0, len(sets) + 1)]
+o = 0
+for set in sets:
+    # Criação do buffer que armazenará os dados antes da escrita.
+    buffer = [[] for i in range(0, size + 1)]
 
-# Para cada um dos
-for path in sets:
-    file = open(path, 'r')
-    sequences = file.readlines()
+    # Para cada um dos caminhos
+    for path in set:
+        file = open(path, 'r')
+        sequences = file.readlines()
 
-    buffer[0].append(sequences[0].strip())
+        buffer[0].append(sequences[0].strip())
 
-    for i in range(0, len(sequences)):
-        sequence = sequences[i].split('; ')
+        for i in range(1, len(sequences)):
+            sequence = sequences[i].split('; ')
 
-        start = time.time()
-        insertion_sort(sequence)
-        end = time.time()
+            start = time.time()
+            insertion_sort(sequence)
+            end = time.time()
 
-        buffer[i+1].append((end - start) * 100)
+            buffer[i].append((end - start) * 100)
 
-# -----------------------------------------------------------
+    # -----------------------------------------------------------
 
-# Para facilitar o tratamento da tabela de tempos, as
-# colunas podem ser escritas de acordo com a ordem 
-# crescente da quantidade de items nas sequências.
+    # Para facilitar o tratamento da tabela de tempos, as
+    # colunas podem ser escritas de acordo com a ordem 
+    # crescente da quantidade de items nas sequências.
 
-# Para isso, basta ordenar a primeira linha do buffer,
-# armazenar os índices ordenados e escrever o arquivo de
-# acordo com essa .
+    # Para isso, basta ordenar a primeira linha do buffer,
+    # armazenar os índices ordenados e escrever o arquivo de
+    # acordo com essa .
 
-# Uma vez que essa etapa não será analisada no teste, 
-# utiliza-se a função de ordenação da própria linguagem.
+    # Uma vez que essa etapa não será analisada no teste, 
+    # utiliza-se a função de ordenação da própria linguagem.
 
-# -----------------------------------------------------------
+    # -----------------------------------------------------------
 
-# Separação e ordenação, em um auxiliar, da primeira linha 
-orderAux = [int(n) for n in buffer[0]]
-orderAux.sort()
+    # Separação e ordenação, em um auxiliar, da primeira linha 
+    orderAux = [int(n) for n in buffer[0]]
+    orderAux.sort()
 
-# Determinação dos índices ordenados
-indexOrder = [buffer[0].index(str(i)) for i in orderAux]
+    # Determinação dos índices ordenados
+    indexOrder = [buffer[0].index(str(i)) for i in orderAux]
 
-# Abertura do arquivo de saída
-output = open(f'./out/insertion.csv', 'w')
+    # Abertura do arquivo de saída
+    output = open(outs[o], 'w')
 
-# Escrita dos dados
-for tuple in buffer:
-    for i in indexOrder:
-        output.write(f'{float(tuple[i]):.5f},')
-    output.write('\n')
+    # Escrita dos dados
+    for tuple in buffer:
+        for i in indexOrder:
+            output.write(f'{float(tuple[i]):.5f},')
+        output.write('\n')
+
+    o = o+1
